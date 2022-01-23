e93f2c6cadefdc3d7cbd4fd2862315859f70a42c
gemaadhan <gemaadhan@gmail.com>
2022-01-24 05:36:44 +0700

    lanjut

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -10,29 +10,29 @@ function searchMovie() {
     },
     // kalo success ngapain
     success: function (hasil) {
-      if (hasil.Response == "True") {
+      if (hasil.Response === "True") {
         let movies = hasil.Search;
         console.log(movies);
         $("#movie-list").html("");
         $.each(movies, function (i, data) {
           $("#movie-list").append(
-            `<div class="col-md-4">
-            <div class="card mb-3" >
-            <img class="card-img-top" src="` +
-              data.Poster +
-              `" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">` +
-              data.Title +
-              `</h5>
-              <h6 class="card-subtitle mb-2 text-muted">` +
-              data.Year +
-              `</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">See Detail</a>
-            </div>
-          </div>
-          </div>`
+            `<div class="col-md-2">
+                <div class="card mb-3" >
+                <img class="card-img-top" src="` +
+                  data.Poster +
+                  `" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">` +
+                  data.Title +
+                  `</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">` +
+                  data.Year +
+                  `</h6>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary see-detail" data-toggle="modal" data-target="#detailModal" data-pengenal="`+ data.imdbID +`">See Detail</a>
+                </div>
+              </div>
+            </div>`
           );
         });
 
@@ -57,8 +57,50 @@ $("#search-button").on("click", function () {
 });
 
 // carikan element search input ketika tombol di lepas (keyup)
-$("#search-input").on("keyup", function (event) {
-  if (event.keyCode === 13) {
+$("#search-input").on("keyup", function (eventnya) {
+  if (eventnya.keyCode === 13) {
     searchMovie();
   }
+});
+
+// carikan saya tombol see-detail ketika tombol di klik lakukan fungsi ini (tampilkan data-pengenal dari tombol yang sedang diklik).. ehhh ternyata ga ketemu element see-detail. karena belum dibuat di halaman index waktu pertama di load, element baru dibuat ketika setelah proses pencarian. 
+$('.see-detail').on("click", function(){
+  console.log($(this).data('pengenal'));
+});
+
+// jadi kita ganti aja, kayak gini. jquery tolong carikan saya element movie-list, kemudian ketika di klik kalo ada element .see-detail entah element nya digenerate nanti, atau dari awal udah ada. 
+$('#movie-list').on("click",'.see-detail', function(){
+  console.log($(this).data('pengenal'));
+  $.ajax({
+    url: "http://omdbapi.com",
+    type: "get",
+    dataType: "json",
+    data : {
+      apikey: "d8980229",
+      i : $(this).data('pengenal')
+    },
+    success: function(hasil){
+      if (hasil.Response === "True") {
+        $(".modal-body").html(`
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-4" align="center">
+            <img src="`+ hasil.Poster +`" class="img-fluid mb-4">
+            </div>
+            <div class="col-md-8">
+                <ul class="list-group">
+                  <li class="list-group-item"><h3>`+hasil.Title+`</h3></li>
+                  <li class="list-group-item">Genre :`+hasil.Genre+`</h3></li>
+                  <li class="list-group-item">Actor :`+hasil.Actors+`</h3></li>
+                  <li class="list-group-item">Plot :`+hasil.Plot+`</h3></li>
+                  </ul>
+            </div>
+          </div>
+        </div>
+        `)
+      }
+    }
+
+
+  })
 });
